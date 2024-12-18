12cfd7bf0d6c089e5107125789a29f6b019c4582
lucas <lucaston2323@gmail.com>
2024-12-18 15:25:37 -0300

    first commit

---
+++ src/test/java/br/com/lucca/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucca.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
