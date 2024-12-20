91c16357fa996193659001d5d20bb081f58a6c50
lucas <lucaston2323@gmail.com>
2024-12-20 16:43:19 -0300

    fix: alterar classe para record

--- src/main/java/br/com/lucca/literalura/models/GutendexResponse.java
+++ src/main/java/br/com/lucca/literalura/models/GutendexResponse.java
@@ -2,41 +2,8 @@ package br.com.lucca.literalura.models;
 
 import java.util.List;
 
-public class GutendexResponse {
-    private int count;
-    private String next;
-    private String previous;
-    private List<Livro> results;
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    public String getNext() {
-        return next;
-    }
-
-    public void setNext(String next) {
-        this.next = next;
-    }
-
-    public String getPrevious() {
-        return previous;
-    }
-
-    public void setPrevious(String previous) {
-        this.previous = previous;
-    }
-
-    public List<Livro> getResults() {
-        return results;
-    }
-
-    public void setResults(List<Livro> results) {
-        this.results = results;
-    }
+public record GutendexResponse(int count,
+                               String next,
+                               String previous,
+                               List<Livro> results) {
 }
