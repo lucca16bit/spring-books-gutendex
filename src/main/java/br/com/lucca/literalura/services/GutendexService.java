0d9979d1caf26be544a94a139c1733590dd9f12d
lucas <lucaston2323@gmail.com>
2024-12-29 15:11:48 -0300

    feat: metódo para buscar livro por titulo na API

--- src/main/java/br/com/lucca/literalura/services/GutendexService.java
+++ src/main/java/br/com/lucca/literalura/services/GutendexService.java
@@ -2,6 +2,7 @@ package br.com.lucca.literalura.services;
 
 import br.com.lucca.literalura.models.GutendexResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URI;
@@ -9,7 +10,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-
+@Service
 public class GutendexService {
     private final String URL_API = "https://gutendex.com/books/";
 
@@ -32,9 +33,23 @@ public class GutendexService {
         }
     }
 
-    public GutendexResponse obterLivros() throws IOException {
-        String json = obterDados();
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(json, GutendexResponse.class);
+    public GutendexResponse buscarLivroPorTitulo(String titulo) throws IOException {
+        String urlBusca = URL_API + "?search=" + titulo.replace(" ", "+");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlBusca))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.readValue(response.body(), GutendexResponse.class);
+            } else {
+                throw new RuntimeException("Erro ao acessar a API Gutendex: " + response.statusCode());
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Erro ao buscar livro: " + e.getMessage(), e);
+        }
     }
 }
