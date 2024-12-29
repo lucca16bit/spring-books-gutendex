f33751afd69113caa3d32261267a353f8a91f900
lucas <lucaston2323@gmail.com>
2024-12-29 15:12:38 -0300

    feat: query para listar livros com autores juntos

--- src/main/java/br/com/lucca/literalura/repository/LivroRepository.java
+++ src/main/java/br/com/lucca/literalura/repository/LivroRepository.java
@@ -2,14 +2,19 @@ package br.com.lucca.literalura.repository;
 
 import br.com.lucca.literalura.models.entity.LivroJPA;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LivroRepository extends JpaRepository<LivroJPA, Long> {
 
     Optional<LivroJPA> findByTitulo(String titulo);
+
+    @Query("SELECT DISTINCT l FROM LivroJPA l LEFT JOIN FETCH l.autores")
+    List<LivroJPA> findAllWithAutores();
 
 //    List<LivroJPA> findByIdioma(String idioma);
 
